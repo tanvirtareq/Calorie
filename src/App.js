fe15d38a97de0b912156a7ce002d8d215110c87b
Tanvir Tareq <cse.tanvir.17@gmail.com>
2022-12-26 12:58:43 +0600

    change according to first review

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
+import FoodEntry from "./food-entry";
 
-import FoodEntry from './FoodEntry';
-
-function App() {
-  return (
-    <FoodEntry/>
-  );
-}
+const App = () => {
+  return <FoodEntry />;
+};
 
 export default App;
